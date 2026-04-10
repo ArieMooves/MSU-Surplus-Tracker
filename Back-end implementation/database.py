d2addb6e3f4216e17f84ddca8c621631d145047f
Aretha Fontaine <arethafontaine21@gmail.com>
2026-04-09 22:50:34 -0500

    Set up SQLite database connection with SQLAlchemy

--- Back-end implementation/database.py
+++ Back-end implementation/database.py
@@ -1,1 +1,21 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
 
+
+# SQLite file will be created automatically in project folder
+DATABASE_URL = "sqlite:///./surplus.db"
+
+# connect_args is required ONLY for SQLite
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"check_same_thread": False}
+)
+
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
+
+Base = declarative_base()
